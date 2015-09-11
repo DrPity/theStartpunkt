d58d4dd54aad2bff6db048f15c7ef3aa282f388c
DrPity <minispam@freenet.de>
2015-09-11 19:57:06 +0200

    some refactoring

--- app/src/index.js
+++ app/src/index.js
@@ -1,11 +1,7 @@
 console.log("Test Jahahahh");
-// var temp = require('./components/home.html');
+
 var fs = require('fs');
 var Vue = require('vue');
-
-// Vue.component('alert', {
-//     template: require('../components/firstTry')
-// });
 
 new Vue({
   el: 'body',
@@ -16,18 +12,3 @@ new Vue({
     home : require('./components/home')
   }
 });
-
-
-// var text = fs.readFile('./home.html','utf8');
-// console.log(text);
-
-
-// fs.readFile('./home.html', function (err, data) {
-//   if (err) throw err;
-//   console.log(data);
-// });
-
-
-
-
-// var test = $.load( "src/components/home.html" );
