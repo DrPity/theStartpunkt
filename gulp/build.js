0870208860dc033c2f063317a97043d915aa2d0b
Michael Schade <schade.mich@gmail.com>
2016-02-09 21:13:26 +0100

    removed duplicated stuff

--- gulp/build.js
+++ gulp/build.js
@@ -1,7 +1,6 @@
 var gulp = require('gulp');
 
 // Watch Files For Changes & Reload
-gulp.task('build', ['clean','styles', 'jshint', 'html', 'browserify',
-  'images', 'fonts', 'extras', 'optimizeImages', 'optimizeHtml'], function() {
+gulp.task('build', ['clean','styles', 'jshint', 'browserify', 'fonts', 'extras', 'optimizeImages', 'optimizeHtml'], function() {
   return gulp.src('dist/**/*');
 });
