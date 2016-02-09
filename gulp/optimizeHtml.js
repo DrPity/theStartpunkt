24bed10e6bc621318b3696436efe149a89aa8815
Michael Schade <schade.mich@gmail.com>
2016-02-09 20:56:53 +0100

    fix in html minifier --> still not good configured

--- gulp/optimizeHtml.js
+++ gulp/optimizeHtml.js
@@ -5,7 +5,7 @@ var gulp = require('gulp'),
 
 gulp.task('optimizeHtml', function() {
 	return gulp.src('app/**/*.html')
-		.pipe(htmlmin({collapseWhitespace: true, removeComments: true }))
+		.pipe(htmlmin({collapseWhitespace: true}))
 		.pipe(replace(/<link href=\"\/styles\/main.css\"[^>]*>/, function(s) {
 			var style = fs.readFileSync('/styles/main.css', 'utf8');
 			return '<style>\n' + style + '\n</style>';
